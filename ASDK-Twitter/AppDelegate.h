6aeca7b66457f21769749e12a9ab95bfd1579250
Aaron Ash <aaron@aaronash.com>
2017-01-08 23:44:49 +0200

    Initial Commit

---
+++ ASDK-Twitter/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  ASDK-Twitter
+//
+//  Created by Aaron Ash on 1/8/17.
+//  Copyright © 2017 Ash Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "RootViewController.h"
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+@property (strong, nonatomic) RootViewController *rootViewController;
+
+
+@end
+
